d67337287d4b1736be29e19a9aece4f6d6f11a0f
Joey <joey@xm8.ninja>
2021-05-20 11:50:39 -0400

    Added querySelectorAll with a forEach loop over NodeList to unselect current active panel so a new active panel will be selected on click

---
+++ script.js
@@ -0,0 +1,15 @@
+// This puts all the .panel divs into a NodeList
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
